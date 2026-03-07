8c8fcbc35042206d1df129efd69f0512027e2ae8
Rex Olis <rex.olis@goabroad.com>
2026-03-07 17:58:34 +0800

    implement tests for the task endpoint

--- tests/Feature/Api/V1/TaskTest.php
+++ tests/Feature/Api/V1/TaskTest.php
@@ -47,4 +47,123 @@ class TaskTest extends TestCase
             ]
         ]);
     }
+
+    public function test_user_can_create_task(): void 
+    {
+        // // Arrange: create a fake task data
+        // $taskData = Task::factory()->make()->toArray();
+
+        // Act: make a post request to the specified endpoint with data
+        $response = $this->postJson('/api/v1/tasks', [
+            'name' => 'Test Task',
+        ]);
+
+        // Assert
+        $response->assertCreated();
+        $response->assertJsonStructure([
+            'data' => ['id', 'name', 'is_completed']
+        ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'name' => 'Test Task',
+        ]);
+    }
+
+    public function test_user_cannot_create_task_without_name(): void 
+    {
+        // // Arrange: create a fake task data
+        // $taskData = Task::factory()->make()->toArray();
+
+        // Act: make a post request to the specified endpoint with data
+        $response = $this->postJson('/api/v1/tasks', [
+            'name' => '',
+        ]);
+
+        // Assert
+        $response->assertStatus(422);
+        $response ->assertJsonValidationErrors(['name']);
+    }
+
+    public function test_user_can_update_task(): void 
+    {
+        // Arrange: create a fake task data
+        $taskData = Task::factory()->create();
+
+        // Act: make a post request to the specified endpoint with data
+        $response = $this->putJson('/api/v1/tasks/' . $taskData->id, [
+            'name' => 'Updated Test Task',
+        ]);
+
+        // Assert
+        $response->assertOk();
+        $response->assertJsonFragment([
+            'name' => 'Updated Test Task',
+        ]);
+    }
+
+    public function test_user_cannot_update_task_with_invalid_data(): void 
+    {
+        // Arrange: create a fake task data
+        $taskData = Task::factory()->create();
+
+        // Act: make a post request to the specified endpoint with data
+        $response = $this->putJson('/api/v1/tasks/' . $taskData->id, [
+            'name' => '',
+        ]);
+
+        // Assert
+        $response->assertStatus(422);
+        $response ->assertJsonValidationErrors(['name']);
+    }
+    
+    public function test_user_can_toggle_task_completion(): void 
+    {
+        // Arrange: create a fake task data
+        $taskData = Task::factory()->create([
+            'is_completed' => false,
+        ]);
+
+        // Act: make a post request to the specified endpoint with data
+        $response = $this->patchJson('/api/v1/tasks/' . $taskData->id . '/complete', [
+            'is_completed' => true,
+        ]);
+
+        // Assert
+        $response->assertOk();
+        $response->assertJsonFragment([
+            'is_completed' => true,
+        ]);
+    }
+    
+    public function test_user_cannot_toggle_task_completion_with_invalid_data(): void 
+    {
+        // Arrange: create a fake task data
+        $taskData = Task::factory()->create([
+            'is_completed' => false,
+        ]);
+
+        // Act: make a post request to the specified endpoint with data
+        $response = $this->patchJson('/api/v1/tasks/' . $taskData->id . '/complete', [
+            'is_completed' => 'yes',
+        ]);
+
+        // Assert
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['is_completed']);
+    }
+    
+    public function test_user_can_delete_task(): void 
+    {
+        // Arrange: create a fake task data
+        $taskData = Task::factory()->create();
+
+        // Act: make a post request to the specified endpoint with data
+        $response = $this->deleteJson('/api/v1/tasks/' . $taskData->id);
+
+        // Assert
+        $response->assertNoContent();
+        $this->assertDatabaseMissing('tasks', [
+            'id' => $taskData->id,
+        ]);
+    }
 }
